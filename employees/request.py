12cecb7c63d34469c757d2c615c5dc46ebf201ef
yasmeencole <yasmeencole1@mail.com>
2021-04-08 11:45:56 -0500

    first commit pushing code to github

---
+++ employees/request.py
@@ -0,0 +1,21 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Kasey",
+        "locationId": 1,
+    },
+    {
+        "id": 2,
+        "name": "Ebony",
+        "locationId": 1,
+    },
+    {
+        "id": 3,
+        "name": "Tia",
+        "locationId": 2,
+    }
+]
+
+
+def get_all_employees():
+    return EMPLOYEES
---
+++ locations/request.py
@@ -0,0 +1,18 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville East",
+    },
+    {
+        "id": 2,
+        "name": "Nashville South",
+    },
+    {
+        "id": 3,
+        "name": "Nashville North",
+    }
+]
+
+
+def get_all_locations():
+    return LOCATIONS
